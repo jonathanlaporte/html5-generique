199e7732c65c14cbcc3001639412a53ce27b6598
Jonathan Laporte <laporte.jonathan@uqam.ca>
2022-11-04 16:05:45 -0400

    scripts de base

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+/*!
+ * Immediately Invoked Function Expression Boilerplate
+ * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
+ */
+;(function () {
+
+    'use strict';
+
+    // Code goes here...
+
+})();
